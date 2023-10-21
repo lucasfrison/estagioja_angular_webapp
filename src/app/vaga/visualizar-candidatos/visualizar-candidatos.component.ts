ae711cac8550adedc716bf8b0323408365b83871
James Rovel Barbosa <diagjames@gmail.com>
2023-10-21 12:44:10 -0300

    T06 - Visualizar vaga html

--- src/app/vaga/visualizar-candidatos/visualizar-candidatos.component.ts
+++ src/app/vaga/visualizar-candidatos/visualizar-candidatos.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-visualizar-candidatos',
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule],
+    MatIconModule,
+    RouterModule],
   templateUrl: './visualizar-candidatos.component.html',
   styleUrls: ['./visualizar-candidatos.component.css']
 })
@@ -24,7 +26,10 @@ export class VisualizarCandidatosComponent implements OnInit{
     { id: 2, titulo: 'Programador AngularJS', imagemUrl: this.caminhoDaImagem },
     { id: 3, titulo: 'Desenvolvimento em React', imagemUrl: this.caminhoDaImagem },
     { id: 4, titulo: 'Vaga Programador Flutter', imagemUrl: this.caminhoDaImagem },
-    { id: 5, titulo: 'Estágio Redes de Computadores', imagemUrl: this.caminhoDaImagem }
+    { id: 5, titulo: 'Estágio Redes de Computadores', imagemUrl: this.caminhoDaImagem },
+    { id: 6, titulo: 'Desenvolvimento em React', imagemUrl: this.caminhoDaImagem },
+    { id: 7, titulo: 'Vaga Programador Fldasdsadutter', imagemUrl: this.caminhoDaImagem },
+    { id: 8, titulo: 'Estágio Redes de Compasdasdasutadores', imagemUrl: this.caminhoDaImagem }
   ];
 
 }
