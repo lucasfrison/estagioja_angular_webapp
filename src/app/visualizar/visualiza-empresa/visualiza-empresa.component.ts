7fa3b218accbcf5adbc6e3f8cdbeb71e401e6b45
James Rovel Barbosa <diagjames@gmail.com>
2023-10-21 23:04:37 -0300

    Hotfix - Remoção snackbar visualizar

--- src/app/visualizar/visualiza-empresa/visualiza-empresa.component.ts
+++ src/app/visualizar/visualiza-empresa/visualiza-empresa.component.ts
@@ -8,7 +8,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { CepService } from 'src/app/services/cep.service';
 import { Empresa } from 'src/app/shared/models/empresa.model';
 import { Endereco } from 'src/app/shared/models/endereco.model';
@@ -47,8 +46,7 @@ export class VisualizaEmpresaComponent implements OnInit {
   }
 
   constructor(
-    private cepService: CepService, 
-    private snackBar: MatSnackBar
+    private cepService: CepService 
   ) {}
 
   public buscarCep()
--- src/app/visualizar/visualizar-estudante/visualizar-estudante.component.ts
+++ src/app/visualizar/visualizar-estudante/visualizar-estudante.component.ts
@@ -9,7 +9,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { CepService } from 'src/app/services/cep.service';
 import { Empresa } from 'src/app/shared/models/empresa.model';
 import { Endereco } from 'src/app/shared/models/endereco.model';
@@ -47,8 +46,7 @@ export class VisualizarEstudanteComponent implements OnInit {
   }
 
   constructor(
-    private cepService: CepService, 
-    private snackBar: MatSnackBar
+    private cepService: CepService
   ) {}
 
   inicializarFormEstudante() {
