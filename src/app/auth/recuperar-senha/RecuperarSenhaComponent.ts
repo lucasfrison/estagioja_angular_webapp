fa044adc05c075be576893dea8b96965e79c4a54
James Rovel Barbosa <diagjames@gmail.com>
2023-10-28 17:45:01 -0300

    T08 - EmailJS

--- src/app/auth/recuperar-senha/RecuperarSenhaComponent.ts
+++ src/app/auth/recuperar-senha/RecuperarSenhaComponent.ts
@@ -3,7 +3,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-recuperar-senha',
@@ -19,10 +19,32 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder } 
 })
 export class RecuperarSenhaComponent implements OnInit {
 
-  constructor(private builder: FormBuilder) { }
+  form: FormGroup = this.fb.group({
+    from_name: 'james',
+    to_name: 'Admin',
+    from_email: 'diagjames@gmail.com',
+    subject: 'dasdsad',
+    message: 'boa tarde teste 123'
+    
+  });
 
-  ngOnInit() {
+  constructor(private builder: FormBuilder, private fb: FormBuilder) { }
 
+  ngOnInit() {}
+
+  async send() {
+    emailjs.init('0yVYnT_golLNxmqYp');// chave api
+    let response = await emailjs.send('service_kyoiey7', 'template_i2i1m6f', {
+      from_name: 'EstagioJa',
+      to_name: 'James',
+      from_email: 'james.govida@hotmail.com',
+      subject: 'Test subject',
+      message: 'this is message', 
+      to_email: 'lucfg15@gmail.com'
+    });
+
+    alert("mensagem enviada!");
+    this.form.reset();
   }
 
 }
